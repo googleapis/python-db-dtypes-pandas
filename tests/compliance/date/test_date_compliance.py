604909137066adbc72d0fac156da684c046f3707
Chalmer Lowe <chalmer.lowe@gmail.com>
2023-06-12 13:03:47 -0400

    Update tests/compliance/date/test_date_compliance.py

--- tests/compliance/date/test_date_compliance.py
+++ tests/compliance/date/test_date_compliance.py
@@ -99,7 +99,7 @@ class TestMethods(base.BaseMethodsTests):
     def test_hash_pandas_object(self):
         pytest.xfail(
             reason="""Causes a breakage in the compliance test suite. Needs
-            further investigation."""
+            further investigation. See issues 182, 183, 185."""
         )
 
 
