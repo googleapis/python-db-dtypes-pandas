2b036627423ea85219b1d4f713583ae79ebfdd91
Chelsea Lin <chelsealin@google.com>
2024-07-16 20:36:58 +0000

    converts to ArrowStringArray

--- db_dtypes/json.py
+++ db_dtypes/json.py
@@ -14,261 +14,237 @@
 
 from __future__ import annotations
 
-from collections import UserDict, abc
-import itertools
-import numbers
-import string
-import sys
-from typing import TYPE_CHECKING, Any
+import typing
 
 import numpy as np
 import pandas as pd
-from pandas.api.extensions import ExtensionArray, ExtensionDtype
-from pandas.core.dtypes.cast import construct_1d_object_array_from_listlike
-from pandas.core.dtypes.common import is_bool_dtype, is_list_like, pandas_dtype
-from pandas.core.indexers import unpack_tuple_and_ellipses
-
-if TYPE_CHECKING:
-    from collections.abc import Mapping
-
-    from pandas._typing import type_t
+from pandas._libs import lib
+from pandas.core.arrays.arrow.array import ArrowExtensionArray
+from pandas.core.arrays.numeric import NumericDtype
+from pandas.core.dtypes.common import is_integer, is_scalar, pandas_dtype
+from pandas.core.dtypes.dtypes import ExtensionDtype
+from pandas.core.indexers import check_array_indexer, unpack_tuple_and_ellipses
+import pyarrow as pa
+import pyarrow.compute as pc
 
 
 @pd.api.extensions.register_extension_dtype
 class JSONDtype(pd.api.extensions.ExtensionDtype):
     """Extension dtype for JSON data."""
 
-    # type = str
-
-    type = abc.Mapping
     name = "dbjson"
-    # na_value = pd.NA  # TODO: StringDtype is libmissing.NA
 
-    na_value: Mapping[str, Any] = UserDict()
-    # _is_numeric = False
-    # _is_boolean = False
+    @property
+    def na_value(self) -> pd.NA:
+        return pd.NA
+
+    @property
+    def type(self) -> type[str]:
+        return str
+
+    @property
+    def _is_numeric(self) -> bool:
+        return False
+
+    @property
+    def _is_boolean(self) -> bool:
+        return False
 
     @classmethod
     def construct_array_type(cls):
         """Return the array type associated with this dtype."""
         return JSONArray
 
-    # @staticmethod
-    # def __from_arrow__(
-    #     array: Union[pyarrow.Array, pyarrow.ChunkedArray]
-    # ) -> "JSONArray":
-    #     """Convert to JSONArray from an Arrow array.
-
-    #     See:
-    #     https://pandas.pydata.org/pandas-docs/stable/development/extending.html#compatibility-with-apache-arrow
-    #     """
-    #     if isinstance(array, pyarrow.Array):
-    #         chunks = [array]
-    #     else:
-    #         chunks = array.chunks
-
-    #     results = []
-    #     for arr in chunks:
-    #         # convert chunk by chunk to numpy and concatenate then, to avoid
-    #         # overflow for large string data when concatenating the pyarrow arrays
-    #         arr = arr.to_numpy(zero_copy_only=False)
-    #         arr = ensure_string_array(arr, na_value=pandas.NA)
-    #         results.append(arr)
-
-    #     if len(chunks) == 0:
-    #         arr = numpy.array([], dtype=str)
-    #     else:
-    #         arr = numpy.concatenate(results)
-
-    #     return JSONArray(arr)
-
-    #     # TODO: codes from StringDtype
-    #     # # Bypass validation inside StringArray constructor, see GH#47781
-    #     # new_string_array = StringArray.__new__(StringArray)
-    #     # NDArrayBacked.__init__(
-    #     #     new_string_array,
-    #     #     arr,
-    #     #     StringDtype(storage="python"),
-    #     # )
-    #     # return new_string_array
+    @staticmethod
+    def __from_arrow__(array: typing.Union[pa.Array, pa.ChunkedArray]) -> JSONArray:
+        """Convert to JSONArray from an Arrow array."""
+        return JSONArray(array)
 
 
-class JSONArray(pd.api.extensions.ExtensionArray):
+class JSONArray(ArrowExtensionArray):
     """Extension array containing JSON data."""
 
-    dtype = JSONDtype()
-    __array_priority__ = 1000
+    _dtype = JSONDtype()
 
     def __init__(self, values, dtype=None, copy=False) -> None:
-        for val in values:
-            if not isinstance(val, self.dtype.type):
-                raise TypeError(
-                    f"All values must be of type {str(self.dtype.type)}: actual {type(val)}"
-                )
-        self.data = values
+        if isinstance(values, (pa.Array, pa.ChunkedArray)) and pa.types.is_string(
+            values.type
+        ):
+            values = pc.cast(values, pa.large_string())
 
-        # Some aliases for common attribute names to ensure pandas supports
-        # these
-        self._items = self._data = self.data
-        # those aliases are currently not working due to assumptions
-        # in internal code (GH-20735)
-        # self._values = self.values = self.data
+        super().__init__(values)
+        self._dtype = JSONDtype()
+
+        if not pa.types.is_large_string(self._pa_array.type) and not (
+            pa.types.is_dictionary(self._pa_array.type)
+            and pa.types.is_large_string(self._pa_array.type.value_type)
+        ):
+            raise ValueError(
+                "ArrowStringArray requires a PyArrow (chunked) array of "
+                "large_string type"
+            )
+
+    @classmethod
+    def _box_pa_scalar(cls, value, pa_type: pa.DataType | None = None) -> pa.Scalar:
+        pa_scalar = super()._box_pa_scalar(value, pa_type)
+        if pa.types.is_string(pa_scalar.type) and pa_type is None:
+            pa_scalar = pc.cast(pa_scalar, pa.large_string())
+        return pa_scalar
+
+    @classmethod
+    def _box_pa_array(
+        cls, value, pa_type: pa.DataType | None = None, copy: bool = False
+    ) -> pa.Array | pa.ChunkedArray:
+        pa_array = super()._box_pa_array(value, pa_type)
+        if pa.types.is_string(pa_array.type) and pa_type is None:
+            pa_array = pc.cast(pa_array, pa.large_string())
+        return pa_array
 
     @classmethod
     def _from_sequence(cls, scalars, *, dtype=None, copy=False):
-        return cls(scalars)
+        from pandas.core.arrays.masked import BaseMaskedArray
+
+        if isinstance(scalars, BaseMaskedArray):
+            # avoid costly conversion to object dtype in ensure_string_array and
+            # numerical issues with Float32Dtype
+            na_values = scalars._mask
+            result = scalars._data
+            result = lib.ensure_string_array(result, copy=copy, convert_na_value=False)
+            return cls(pa.array(result, mask=na_values, type=pa.large_string()))
+        elif isinstance(scalars, (pa.Array, pa.ChunkedArray)):
+            return cls(pc.cast(scalars, pa.large_string()))
+
+        # convert non-na-likes to str
+        result = lib.ensure_string_array(scalars, copy=copy)
+        return cls(pa.array(result, type=pa.large_string(), from_pandas=True))
+
+    @classmethod
+    def _from_sequence_of_strings(
+        cls, strings, *, dtype: ExtensionDtype, copy: bool = False
+    ) -> JSONArray:
+        return cls._from_sequence(strings, dtype=dtype, copy=copy)
+
+    @property
+    def dtype(self) -> JSONDtype:
+        """An instance of JSONDtype"""
+        return self._dtype
+
+    def insert(self, loc: int, item) -> JSONArray:
+        if not isinstance(item, str) and not pd.isna(item):
+            raise TypeError("Scalar must be NA or str")
+        return super().insert(loc, item)
+
+    def astype(self, dtype, copy: bool = True):
+        dtype = pandas_dtype(dtype)
+
+        if dtype == self.dtype:
+            if copy:
+                return self.copy()
+            return self
+        elif isinstance(dtype, NumericDtype):
+            data = self._pa_array.cast(pa.from_numpy_dtype(dtype.numpy_dtype))
+            return dtype.__from_arrow__(data)
+        elif isinstance(dtype, np.dtype) and np.issubdtype(dtype, np.floating):
+            return self.to_numpy(dtype=dtype, na_value=np.nan)
+
+        return super().astype(dtype, copy=copy)
 
     @classmethod
     def _from_factorized(cls, values, original):
-        return cls([UserDict(x) for x in values if x != ()])
+        return cls._from_sequence(values, dtype=original.dtype)
 
     def __getitem__(self, item):
-        if isinstance(item, tuple):
+        """Select a subset of self.
+
+        Parameters
+        ----------
+        item : int, slice, or ndarray
+            * int: The position in 'self' to get.
+            * slice: A slice object, where 'start', 'stop', and 'step' are
+              integers or None
+            * ndarray: A 1-d boolean NumPy ndarray the same length as 'self'
+
+        Returns
+        -------
+        item : scalar or ExtensionArray
+
+        Notes
+        -----
+        For scalar ``item``, return a scalar value suitable for the array's
+        type. This should be an instance of ``self.dtype.type``.
+        For slice ``key``, return an instance of ``ExtensionArray``, even
+        if the slice is length 0 or 1.
+        For a boolean mask, return an instance of ``ExtensionArray``, filtered
+        to the values where ``item`` is True.
+        """
+        item = check_array_indexer(self, item)
+
+        if isinstance(item, np.ndarray):
+            if not len(item):
+                return type(self)(pa.chunked_array([], type=pa.string()))
+            elif item.dtype.kind in "iu":
+                return self.take(item)
+            elif item.dtype.kind == "b":
+                return type(self)(self._pa_array.filter(item))
+            else:
+                raise IndexError(
+                    "Only integers, slices and integer or "
+                    "boolean arrays are valid indices."
+                )
+        elif isinstance(item, tuple):
             item = unpack_tuple_and_ellipses(item)
 
-        if isinstance(item, numbers.Integral):
-            return self.data[item]
-        elif isinstance(item, slice) and item == slice(None):
-            # Make sure we get a view
-            return type(self)(self.data)
-        elif isinstance(item, slice):
-            # slice
-            return type(self)(self.data[item])
-        elif not is_list_like(item):
+        if is_scalar(item) and not is_integer(item):
             # e.g. "foo" or 2.5
             # exception message copied from numpy
             raise IndexError(
                 r"only integers, slices (`:`), ellipsis (`...`), numpy.newaxis "
                 r"(`None`) and integer or boolean arrays are valid indices"
             )
-        else:
-            item = pd.api.indexers.check_array_indexer(self, item)
-            if is_bool_dtype(item.dtype):
-                return type(self)._from_sequence(
-                    [x for x, m in zip(self, item) if m], dtype=self.dtype
-                )
-            # integer
-            return type(self)([self.data[i] for i in item])
+        # We are not an array indexer, so maybe e.g. a slice or integer
+        # indexer. We dispatch to pyarrow.
+        if isinstance(item, slice):
+            # Arrow bug https://github.com/apache/arrow/issues/38768
+            if item.start == item.stop:
+                pass
+            elif (
+                item.stop is not None
+                and item.stop < -len(self)
+                and item.step is not None
+                and item.step < 0
+            ):
+                item = slice(item.start, None, item.step)
 
-    def __setitem__(self, key, value) -> None:
-        if isinstance(key, numbers.Integral):
-            self.data[key] = value
+        value = self._pa_array[item]
+        if isinstance(value, pa.ChunkedArray):
+            return type(self)(value)
         else:
-            if not isinstance(value, (type(self), abc.Sequence)):
-                # broadcast value
-                value = itertools.cycle([value])
-
-            if isinstance(key, np.ndarray) and key.dtype == "bool":
-                # masking
-                for i, (k, v) in enumerate(zip(key, value)):
-                    if k:
-                        assert isinstance(v, self.dtype.type)
-                        self.data[i] = v
+            scalar = value.as_py()
+            if scalar is None:
+                return self._dtype.na_value
             else:
-                for k, v in zip(key, value):
-                    assert isinstance(v, self.dtype.type)
-                    self.data[k] = v
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __eq__(self, other):
-        return NotImplemented
-
-    def __ne__(self, other):
-        return NotImplemented
-
-    def __array__(self, dtype=None, copy=None):
-        if dtype is None:
-            dtype = object
-        if dtype == object:
-            # on py38 builds it looks like numpy is inferring to a non-1D array
-            return construct_1d_object_array_from_listlike(list(self))
-        return np.asarray(self.data, dtype=dtype)
-
-    @property
-    def nbytes(self) -> int:
-        return sys.getsizeof(self.data)
-
-    def isna(self):
-        return np.array([x == self.dtype.na_value for x in self.data], dtype=bool)
-
-    def take(self, indexer, allow_fill=False, fill_value=None):
-        # re-implement here, since NumPy has trouble setting
-        # sized objects like UserDicts into scalar slots of
-        # an ndarary.
-        indexer = np.asarray(indexer)
-        msg = (
-            "Index is out of bounds or cannot do a "
-            "non-empty take from an empty array."
-        )
-
-        if allow_fill:
-            # Do not allow any custom na_value
-            if fill_value is None:
-                fill_value = self.dtype.na_value
-            # bounds check
-            if (indexer < -1).any():
-                raise ValueError
-            try:
-                output = [
-                    self.data[loc] if loc != -1 else fill_value for loc in indexer
-                ]
-            except IndexError as err:
-                raise IndexError(msg) from err
-        else:
-            try:
-                output = [self.data[loc] for loc in indexer]
-            except IndexError as err:
-                raise IndexError(msg) from err
-
-        return type(self)._from_sequence(output, dtype=self.dtype)
-
-    def copy(self):
-        return type(self)(self.data[:])
-
-    def astype(self, dtype, copy=True):
-        # NumPy has issues when all the dicts are the same length.
-        # np.array([UserDict(...), UserDict(...)]) fails,
-        # but np.array([{...}, {...}]) works, so cast.
-        from pandas.core.arrays.string_ import StringDtype
-
-        dtype = pandas_dtype(dtype)
-        # needed to add this check for the Series constructor
-        if isinstance(dtype, type(self.dtype)) and dtype == self.dtype:
-            if copy:
-                return self.copy()
-            return self
-        elif isinstance(dtype, StringDtype):
-            value = self.astype(str)  # numpy doesn't like nested dicts
-            arr_cls = dtype.construct_array_type()
-            return arr_cls._from_sequence(value, dtype=dtype, copy=False)
-        elif not copy:
-            return np.asarray([dict(x) for x in self], dtype=dtype)
-        else:
-            return np.array([dict(x) for x in self], dtype=dtype, copy=copy)
-
-    def unique(self):
-        # Parent method doesn't work since np.array will try to infer
-        # a 2-dim object.
-        return type(self)([dict(x) for x in {tuple(d.items()) for d in self.data}])
+                return scalar
 
     @classmethod
-    def _concat_same_type(cls, to_concat):
-        data = list(itertools.chain.from_iterable(x.data for x in to_concat))
-        return cls(data)
+    def _result_converter(cls, values, na=None):
+        return pd.BooleanDtype().__from_arrow__(values)
 
-    def _values_for_factorize(self):
-        frozen = self._values_for_argsort()
-        if len(frozen) == 0:
-            # factorize_array expects 1-d array, this is a len-0 2-d array.
-            frozen = frozen.ravel()
-        return frozen, ()
+    @classmethod
+    def _concat_same_type(cls, to_concat) -> JSONArray:
+        """
+        Concatenate multiple JSONArray.
 
-    def _values_for_argsort(self):
-        # Bypass NumPy's shape inference to get a (N,) array of tuples.
-        frozen = [tuple(x.items()) for x in self]
-        return construct_1d_object_array_from_listlike(frozen)
+        Parameters
+        ----------
+        to_concat : sequence of JSONArray
+
+        Returns
+        -------
+        JSONArray
+        """
+        chunks = [array for ea in to_concat for array in ea._pa_array.iterchunks()]
+        arr = pa.chunked_array(chunks, type=pa.large_string())
+        return cls(arr)
 
     def _pad_or_backfill(self, *, method, limit=None, copy=True):
         # GH#56616 - test EA method without limit_area argument
--- tests/compliance/json/conftest.py
+++ tests/compliance/json/conftest.py
@@ -12,42 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
-from collections import UserDict, abc
-import operator
-import string
-import sys
-from typing import TYPE_CHECKING, Any
+
+import json
 
 import numpy as np
 import pandas as pd
 import pandas._testing as tm
-from pandas.tests.extension import base
 import pytest
 
 from db_dtypes import JSONArray, JSONDtype
 
-if TYPE_CHECKING:
-    from collections.abc import Mapping
-
 
 def make_data():
-    # TODO: Use a regular dict. See _NDFrameIndexer._setitem_with_indexer
-    rng = np.random.default_rng(2)
-    return [
-        UserDict(
-            [
-                (rng.choice(list(string.ascii_letters)), rng.integers(0, 100))
-                for _ in range(rng.integers(0, 10))
-            ]
-        )
-        for _ in range(100)
+    # Sample data with varied lengths.
+    samples = [
+        {"id": 1, "bool_value": True},  # Boolean
+        {"id": 2, "float_num": 3.14159},  # Floating
+        {"id": 3, "date": "2024-07-16"},  # Dates (as strings)
+        {"id": 4, "null_field": None},  # Null
+        {"list_data": [10, 20, 30]},  # Lists
+        {"person": {"name": "Alice", "age": 35}},  # Nested objects
+        {"address": {"street": "123 Main St", "city": "Anytown"}},
+        {"order": {"items": ["book", "pen"], "total": 15.99}},
     ]
-
-
-# We intentionally don't run base.BaseSetitemTests because pandas'
-# internals has trouble setting sequences of values into scalar positions.
-unhashable = pytest.mark.xfail(reason="Unhashable")
+    return np.random.default_rng(2).choice(samples, size=100)
 
 
 @pytest.fixture
@@ -67,9 +55,10 @@ def data():
     # the first two elements, so that's what we'll check.
 
     while len(data[0]) == len(data[1]):
+        print(data)
         data = make_data()
 
-    return JSONArray(data)
+    return JSONArray._from_sequence(data)
 
 
 @pytest.fixture
@@ -79,47 +68,56 @@ def data_for_twos(dtype):
 
     Call pytest.skip in your fixture if the dtype does not support divmod.
     """
-    if not (dtype._is_numeric or dtype.kind == "m"):
-        # Object-dtypes may want to allow this, but for the most part
-        #  only numeric and timedelta-like dtypes will need to implement this.
-        pytest.skip(f"{dtype} is not a numeric dtype")
-
-    raise NotImplementedError
+    pytest.skip(f"{dtype} is not a numeric dtype")
 
 
 @pytest.fixture
 def data_missing():
     """Length 2 array with [NA, Valid]"""
-    return JSONArray([{}, {"a": 10}])
+    return JSONArray._from_sequence([None, {"a": 10}])
 
 
 @pytest.fixture
 def data_for_sorting():
-    return JSONArray([{"b": 1}, {"c": 4}, {"a": 2, "c": 3}])
+    return JSONArray._from_sequence(
+        [json.dumps({"b": 1}), json.dumps({"c": 4}), json.dumps({"a": 2, "c": 3})]
+    )
 
 
 @pytest.fixture
 def data_missing_for_sorting():
-    return JSONArray([{"b": 1}, {}, {"a": 4}])
+    return JSONArray._from_sequence([json.dumps({"b": 1}), None, json.dumps({"a": 4})])
 
 
 @pytest.fixture
 def na_cmp():
-    return operator.eq
+    """
+    Binary operator for comparing NA values.
+
+    Should return a function of two arguments that returns
+    True if both arguments are (scalar) NA for your type.
+
+    By default, uses ``operator.is_``
+    """
+
+    def cmp(a, b):
+        return lambda left, right: pd.isna(left) and pd.isna(right)
+
+    return cmp
 
 
 @pytest.fixture
 def data_for_grouping():
-    return JSONArray(
+    return JSONArray._from_sequence(
         [
-            {"b": 1},
-            {"b": 1},
-            {},
-            {},
-            {"a": 0, "c": 2},
-            {"a": 0, "c": 2},
-            {"b": 1},
-            {"c": 2},
+            json.dumps({"b": 1}),
+            json.dumps({"b": 1}),
+            None,
+            None,
+            json.dumps({"a": 0, "c": 2}),
+            json.dumps({"a": 0, "c": 2}),
+            json.dumps({"b": 1}),
+            json.dumps({"c": 2}),
         ]
     )
 
@@ -199,14 +197,6 @@ def all_arithmetic_operators(request):
     Fixture for dunder names for common arithmetic operations.
     """
     return request.param
-
-
-@pytest.fixture
-def na_value():
-    """
-    The scalar missing value for this type. Default 'None'.
-    """
-    return UserDict()
 
 
 @pytest.fixture(params=["data", "data_missing"])
