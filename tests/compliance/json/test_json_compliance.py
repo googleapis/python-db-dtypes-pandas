d11cc873756f859d55933e8539c52b89a089050d
Chelsea Lin <chelsealin@google.com>
2024-07-22 22:45:24 +0000

    add docstring and remove unused functions

--- tests/compliance/json/test_json_compliance.py
+++ tests/compliance/json/test_json_compliance.py
@@ -26,29 +26,6 @@ from db_dtypes import JSONArray
 
 
 class TestJSONArray(base.ExtensionTests):
-    @pytest.mark.parametrize(
-        "limit_area, input_ilocs, expected_ilocs",
-        [
-            ("outside", [1, 0, 0, 0, 1], [1, 0, 0, 0, 1]),
-            ("outside", [1, 0, 1, 0, 1], [1, 0, 1, 0, 1]),
-            ("outside", [0, 1, 1, 1, 0], [0, 1, 1, 1, 1]),
-            ("outside", [0, 1, 0, 1, 0], [0, 1, 0, 1, 1]),
-            ("inside", [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]),
-            ("inside", [1, 0, 1, 0, 1], [1, 1, 1, 1, 1]),
-            ("inside", [0, 1, 1, 1, 0], [0, 1, 1, 1, 0]),
-            ("inside", [0, 1, 0, 1, 0], [0, 1, 1, 1, 0]),
-        ],
-    )
-    def test_ffill_limit_area(
-        self, data_missing, limit_area, input_ilocs, expected_ilocs
-    ):
-        # GH#56616
-        msg = "JSONArray does not implement limit_area"
-        with pytest.raises(NotImplementedError, match=msg):
-            super().test_ffill_limit_area(
-                data_missing, limit_area, input_ilocs, expected_ilocs
-            )
-
     @pytest.mark.xfail(reason="Unhashable")
     def test_value_counts_with_normalize(self, data):
         super().test_value_counts_with_normalize(data)
