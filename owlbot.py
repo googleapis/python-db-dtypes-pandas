21fa56d9d761b9166b3c8e59e34b048f9344a189
Chalmer Lowe <chalmer.lowe@gmail.com>
2025-06-18 13:24:41 -0400

    Update owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -43,7 +43,7 @@ s.move(
         ".github/workflows/docs.yml", # to avoid overwriting python version
         ".github/workflows/lint.yml", # to avoid overwriting python version
         "noxfile.py",
-        "renovate.json",
+        "renovate.json", # to avoid overwriting the ignorePaths list.
     ]
 )
 
