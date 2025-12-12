abe2b62415f639ce8f682e40661e75ac664b1ab8
Daniel Sanche <d.sanche14@gmail.com>
2025-12-12 14:13:00 -0800

    moved pragma

--- tests/unit/test_pandas_backports.py
+++ tests/unit/test_pandas_backports.py
@@ -27,13 +27,13 @@ def _import_side_effect(module_name, result_return=None, result_raise=None):
     arguments. Otherwise, it will default to regular import behaviour
     """
 
-    def _impl(name, *args, **kwargs):  # pragma: no cover
+    def _impl(name, *args, **kwargs):
         if name == module_name:
             if result_raise:
                 raise result_raise
             else:
                 return result_return
-        else:
+        else:  # pragma: no cover
             return REAL_IMPORT(name, *args, **kwargs)
 
     return _impl
